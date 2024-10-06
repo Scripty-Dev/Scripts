baeaf6785bd0fc6607fee175cf4ce733a27b5420
Alex Wu <ch33zig@gmail.com>
2024-10-06 12:35:22 -0400

    Update google.ts

--- google.ts
+++ google.ts
@@ -11,7 +11,7 @@ export const func = async ({ query }: { query: string }) => {
     })
 
     const response = await request.json()
-    return response
+    return JSON.stringify(response)
 }
 
 export const object = {

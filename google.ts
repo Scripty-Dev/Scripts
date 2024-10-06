9f995f4b82862880645a8a36260dbd8ca627907b
Alex Wu <ch33zig@gmail.com>
2024-10-06 12:26:44 -0400

    Update google.ts

--- google.ts
+++ google.ts
@@ -1,19 +1,17 @@
 export const func = async ({ query }: { query: string }) => {
-    const request = await fetch('https://google.serper.dev/search', {
+    const request = await fetch('https://scripty.me/api/assistant/serper', {
         method: 'POST',
         headers: {
-            'X-API-KEY': config['serper-api-key'],
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            q: query,
+            query,
+            token: config['token'],
         }),
     })
 
     const response = await request.json()
-    return response.answerBox
-        ? JSON.stringify(response.answerBox)
-        : JSON.stringify(response.organic)
+    return response
 }
 
 export const object = {
